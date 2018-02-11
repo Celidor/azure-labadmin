d8e54d1800fa4f814b4cc8cb2bd7c02821a25084
paulschwarzenberger <paul@celidor.net>
2018-02-11 12:23:34 +0000

    script to disable password expiry

---
+++ windows/disable-password-expiry.ps1
@@ -0,0 +1,12 @@
+#disable password expiry on Azure AD account
+#prerequisites:
+#Install the 64-bit version of the Microsoft Online Services Sign-in Assistant at https://www.microsoft.com/en-us/download/details.aspx?id=41950
+#Install-Module MSOnline
+#set-executionpolicy unrestricted
+Import-Module MSOnline
+Connect-MsolService
+for ($i=1;$i -le 14; $i++) {
+    "csa$i@celidor.net"
+    Set-MsolUser -UserPrincipalName csa$i@celidor.net -PasswordNeverExpires $true
+    Get-MSOLUser -UserPrincipalName csa$i@celidor.net | Select PasswordNeverExpires
+}
